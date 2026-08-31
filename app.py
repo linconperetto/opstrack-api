1c835fed79d181b38700e48fe4018c6b04a81c27
Lincon Peretto <linconperetto@gmail.com>
2026-08-31 08:05:24 -0300

    fix: correção do api status

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def status():
-    return {'servico': 'OpsTrackAPI', 'status': 'online'}
+    return {'servico': 'OpsTrackAPI', 'status': 'active'}
 
 if __name__ == '__main__':
     app.run(debug=True)
